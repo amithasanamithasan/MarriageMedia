a7d580d2d4ab8c428c119139a63218960ece9ed8
amithasanamithasan <amit35-1773@diu.edu.bd>
2020-10-26 00:52:34 +0600

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Welocome to Laravel MarrigeMedia Project</title>
+        <title>Welocome to Laravel MarriageMedia Project</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Welocome to Laravel MarrigeMedia Project
+                   Welocome to Laravel MarriageMedia Project
                 </div>
 
                 <div class="links">
